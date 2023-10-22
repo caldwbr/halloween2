c53257d341e92d114b6098ce849c949bb66febc9
caldwbr <caldwbr@gmail.com>
2023-10-22 14:45:30 -0500

    Initial Commit

---
+++ halloween2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  halloween2
+//
+//  Created by Brad Caldwell on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ halloween2/halloween2App.swift
@@ -0,0 +1,17 @@
+//
+//  halloween2App.swift
+//  halloween2
+//
+//  Created by Brad Caldwell on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct halloween2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
